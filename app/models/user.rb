a3a4dfdba7279720675a3e2b591867f0831c950a
Roman <sunkevu4@gmail.com>
2021-01-08 10:25:39 +0200

    add rubocop, refactor code according to the rubocop rules (#19)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
     self.emoji = rand(62) unless self.emoji
     self.visits = 1 unless self.visits
   end
-  
+
   validates :name, presence: true, length: { minimum: 3 }
   validates :ip_address, presence: true
   validates :device, presence: true
@@ -14,13 +14,13 @@ class User < ApplicationRecord
   scope :filter_by_last_seen, -> { order('last_seen_at').reverse_order }
   # scope :online, -> { where(online: true) }
   # scope :offline, -> { where(online: false) }
-  
+
   # after user makes any request, update visits and last visit date
   def refresh_activity
     visits = user_online? ? self.visits + 1 :  self.visits
     self.update(visits: visits, last_seen_at: Time.now.to_i)
   end
-  
+
   # if the user was on the page more than five minutes ago update visits
   def user_online?
     last_visit = self.last_seen_at + DEFAULT_TIME
@@ -31,16 +31,16 @@ class User < ApplicationRecord
     end
   end
 
-  # if a user appears on the site, we send information about him to all online users 
+  # if a user appears on the site, we send information about him to all online users
   def appear
     self.update(online: true)
-    ActionCable.server.broadcast "activity_channel", status: 'online', user_name: self.name 
+    ActionCable.server.broadcast 'activity_channel', status: 'online', user_name: self.name
   end
-  
+
   # if the user closes the site tab, we send the data of this user to all those who are online
   def away
     self.update(online: false, last_seen_at: Time.now.to_i)
-    ActionCable.server.broadcast "activity_channel", status: 'offline',
+    ActionCable.server.broadcast 'activity_channel', status: 'offline',
                                                      user_name: self.name,
                                                      user_away_at: self.last_seen_at
   end
